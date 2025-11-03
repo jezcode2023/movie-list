44db77cc31284cb27f111011d221971b5410e62d
Jezreel Ortiz <jezreelortiz.140934@gmail.com>
2025-11-03 23:29:22 +0800

    Delete webpack.config.js

--- config/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const WorkboxPlugin = require('workbox-webpack-plugin');
-
-plugins: [
-  // other plugins...
-  new WorkboxPlugin.GenerateSW({
-    clientsClaim: true,
-    skipWaiting: true,
-  }),
-];
